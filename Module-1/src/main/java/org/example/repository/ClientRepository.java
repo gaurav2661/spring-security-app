c379fcca20ecfc30e9c94a29a87600b2bd1c5593
Gaurav58.singh@ril.com <gaurav.2661@gmail.com>
2024-01-02 16:30:01 +0530

    added new dependencies

--- Module-1/src/main/java/org/example/repository/ClientRepository.java
+++ Module-1/src/main/java/org/example/repository/ClientRepository.java
@@ -2,6 +2,11 @@ package org.example.repository;
 
 import org.example.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ClientRepository extends JpaRepository<Client,Integer> {
+    Optional<Client> findByEmail(String email);
 }
